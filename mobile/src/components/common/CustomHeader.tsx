5817dfdedb99d02fb70976c450c116c588375ecc
Shahbaz Husain Khan <khanshahbazhusain@gmail.com>
2025-11-17 18:40:56 +0530

    add custom header

--- mobile/src/components/common/CustomHeader.tsx
+++ mobile/src/components/common/CustomHeader.tsx
@@ -1,10 +1,24 @@
 import { View, Text } from 'react-native';
 import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import { goBack, navigate } from '../../navigation/NavigationUtils';
+import { ChevronLeft } from 'lucide-react-native';
+import { Ellipse } from 'react-native-svg';
 
 const CustomHeader = ({title} :{ title: string}) => {
   return (
-    <View className="flex-row items-center justify-center h-16 px-3 bg-white border-b border-light-border dark:border-dark-border dark:bg-dark-50">
+    <View className="flex-row items-center justify-between h-16 px-3 bg-white border-b border-light-border dark:border-dark-border dark:bg-dark-50">
+        <TouchableOpacity className="items-center justify-center w-10 h-10 rounded-full shadow-2xl bg-light shadow-light-border dark:shadow-dark-border dark:bg-dark-50"
+        onPress={()=>goBack()}
+        >
+          <ChevronLeft size={20} color="#005FFF"/>
+        </TouchableOpacity>
         <Text className="text-lg text-black dark:text-white font-rubik-extrabold">{title}</Text>
+        <TouchableOpacity className="items-center justify-center w-10 h-10 rounded-full shadow-2xl bg-light shadow-light-border dark:shadow-dark-border dark:bg-dark-50"
+        onPress={()=>navigate('RequestScreen')}
+        >
+          <Ellipse size={20} color="#005FFF"/>
+        </TouchableOpacity>
       </View>
   );
 };
