7d644cfba72d0970efcef810eb46c653eec41f18
Shahbaz Husain Khan <khanshahbazhusain@gmail.com>
2026-02-28 15:35:11 +0530

    setup socket io

--- mobile/src/navigation/MainNavigator.tsx
+++ mobile/src/navigation/MainNavigator.tsx
@@ -1,14 +1,17 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { useColorScheme } from 'nativewind';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ActivityIndicator, StatusBar } from 'react-native';
 import { ToastProvider } from 'react-native-toast-notifications';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useCurrentUserQuery } from '../redux/api/userApi';
 import { userData } from '../redux/slice/userSlice';
 import { navigationRef } from './NavigationUtils';
 import StackNavigator from './StackNavigator';
+import { RootState } from '../redux/store';
+import io from 'socket.io-client';
 const MainNavigator = () => {
+  const authUser = useSelector((state:RootState)=>state.auth.user);
   const { colorScheme} = useColorScheme();
   const dispatch = useDispatch();
   const {data, isLoading:currentDataLoading} = useCurrentUserQuery();
@@ -20,9 +23,20 @@ const MainNavigator = () => {
         }
   // eslint-disable-next-line react-hooks/exhaustive-deps
   },[data, dispatch]);
-if(currentDataLoading){
-  return <ActivityIndicator/>;
-}
+
+  const [socket, setSocket] = useState(null);
+  useEffect(()=>{
+    if(authUser){
+      const socket = io('http://10.100.38.250:4000',{
+
+      });
+      setSocket(socket);
+    }
+  }, [authUser]);
+
+  if(currentDataLoading){
+    return <ActivityIndicator/>;
+  }
   return (
     <NavigationContainer
     ref={navigationRef}
